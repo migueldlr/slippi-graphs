f18bf57d8056d107aa51abda7ae0233b72d9f196
mbclr3 <mbclr3@gatech.edu>
2021-05-10 16:03:29 -0400

    Add GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,32 @@
 // import '../util/wdyr';
+import { useEffect } from 'react';
 import '../styles/globals.css';
+import { useRouter } from 'next/router';
+
+const pageview = url => {
+  window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+    page_path: url,
+  });
+};
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
